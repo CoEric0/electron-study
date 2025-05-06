263761d350136df724b7661cd24d8fe4d6ca28a8
zmba <wszmc99@gmail.com>
2025-05-06 15:40:34 +0800

    删除部分组件，方便修改

--- example/react2/src/App.tsx
+++ example/react2/src/App.tsx
@@ -1,12 +1,15 @@
-import { DefaultComponent } from "./components/hello"
+import { BrowserRouter, Routes, Route } from "react-router-dom"
 import { Welcome } from "./components/welcome"
+
+
+import HomePage from "./components/HomePage"
+import SecondPage from "./components/SecondPage"
 
 function App() {
   return (
     // React Fragment, 常用于包裹多个元素
     <>
       <Welcome />
-      <DefaultComponent/>
     </>
   )
 }
