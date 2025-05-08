ce8b8f8cd9cce671b063b49b63ab970642a26273
zmba <wszmc99@gmail.com>
2025-05-08 09:51:45 +0800

    准备step2，将前端植入react

---
+++ example/step2/src/main/index.ts
@@ -0,0 +1,71 @@
+import { app, BrowserWindow, ipcMain } from 'electron';
+import path from 'node:path';
+import { get_ping_num, ping } from './service/ping';
+
+
+console.log(process.env.VITE_DEV_SERVER_URL)
+console.log(process.env['ELECTRON_RENDERER_URL'])
+console.log(process.env.NODE_ENV)
+
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, '../preload/index.js'), // 这里应当指向编译后的js文件
+      contextIsolation: true, // 推荐开启上下文隔离
+      nodeIntegration: false, // 推荐关闭 Node.js 集成
+    },
+  });
+
+
+  // 进程间通讯注册
+  ipcMain.on('message-from-renderer', (event, message) => {
+    console.log('Received message from renderer:', message);
+  });
+  ipcMain.on('message-from-stand', (event, message) => {
+    console.log('Received message from stand:', message);
+  });
+  ipcMain.handle('ping', async (event) => {
+    console.log('Received ping request from stand');
+    const ping_value = await get_ping_num();
+    console.log('Ping value:', ping_value);
+
+    return ping_value;
+  }
+  );
+
+
+  // VITE_DEV_SERVER_URL 已过时
+  // const devServerUrl = process.env.VITE_DEV_SERVER_URL || 'http://localhost:5173';
+  // ELECTRON_RENDERER_URL 是最新的
+  const devServerUrl = process.env.ELECTRON_RENDERER_URL || 'http://localhost:5173';
+  // 加载渲染进程的 HTML 文件
+  if (process.env.NODE_ENV !== 'production') {
+    win.loadURL(devServerUrl);
+    win.webContents.openDevTools();
+  } else {
+    // 生产模式下加载打包后的 HTML
+    win.loadFile(path.join(__dirname, '../../src/renderer/index.html'));
+  }
+}
+
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on('activate', () => {
+    // 在 macOS 上，当单击 dock 图标并且没有其他窗口打开时，
+    // 通常在应用程序中重新创建一个窗口。
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow();
+    }
+  });
+});
+
+app.on('window-all-closed', () => {
+  // 在 macOS 外，当所有窗口都关闭时退出应用程序。
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
---
+++ example/step2/src/renderer/main.ts
@@ -0,0 +1,66 @@
+import { ipcRenderer } from 'electron';
+
+// 这是一个简单的渲染进程脚本示例
+
+// 获取 HTML 元素并添加事件监听器
+// const pingResultElement = document.getElementById('ping-result');
+// if (pingResultElement && window.myAPI) {
+//   console.log('Calling preload API...');
+//   try {
+//     const result = window.myAPI.ping();
+//     pingResultElement.textContent = result;
+//   } catch (error) {
+//     console.error('Error calling preload API:', error);
+//     pingResultElement.textContent = 'Error!';
+//   }
+// } else {
+//   if (!pingResultElement) console.error('Element #ping-result not found');
+//   if (!window.myAPI) console.error('window.myAPI is not defined, check preload script and contextBridge.');
+//   if(pingResultElement) pingResultElement.textContent = 'API not available';
+// }
+
+
+console.log('Renderer script loaded.');
+
+
+// 预加载脚本的使用
+const Elec_verison = document.getElementById('elec-version');
+console.log(myAPI.version);
+if (Elec_verison) {
+  Elec_verison.textContent = myAPI.version;
+}
+
+// 前端向后段传递信息
+const sendButton = document.getElementById('send-button');
+const inputMessage = document.getElementById('input');
+if (sendButton && inputMessage) {
+    // 更现代的事件监听方法：多个监听、事件捕获、移除监听
+    // 事件类型，回调函数（无参数）
+    sendButton.addEventListener('click', () => {
+        const message = inputMessage.value;
+        console.log('Sending message:', message);
+        // 直接向主进程发送消息
+        window.electron.ipcRenderer.send('message-from-renderer', message);
+        // 通过预加载脚本的 API
+        myAPI.r2m_message(message);
+    });
+}
+
+// 后端向前端传递信息(双向)
+const pingText = document.getElementById('ping-result');
+const pingButton = document.getElementById('ping-button');
+if (pingText && pingButton) {
+    pingButton.addEventListener('click', async () => {
+        // 更新页面
+        if (pingText) pingText.textContent = 'Loading...';
+        console.log('Fetching ping message from main process');
+
+        let p = await myAPI.ping_check()
+        pingText.textContent = p;
+        console.log('Ping message:', p);
+    });
+}
+
+
+
+// 你可以在这里添加更多的前端逻辑，例如使用 React, Vue 等框架
